810c30b1525863fba8453a349358b8b137284232
Jinu Choi <jinujinu99@gmail.com>
2022-07-25 00:51:50 +0900

    change css to control page image

--- client/src/Components/PageViewer/pageViewer.js
+++ client/src/Components/PageViewer/pageViewer.js
@@ -1,10 +1,10 @@
 import './pageViewer.css';
 
 export const PageViewer = (props) => {
-    if(props.num > 0 && props.data.length >= props.num)
-    return(
-        <>
-            <img src={props.data[props.num - 1].image}/>
-        </>
-    )
-}
+  if (props.num > 0 && props.data.length >= props.num)
+    return (
+      <div>
+        <img src={props.data[props.num - 1].image} />
+      </div>
+    );
+};
--- client/src/Pages/PlayPage.js
+++ client/src/Pages/PlayPage.js
@@ -1,55 +1,57 @@
-import React, { useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
-import { useLocation } from 'react-router-dom'
-import { Navbar } from '../Components/Navbar/navbar'
-import { Infobar } from '../Components/Infobar/infobar'
-import { PageViewer } from '../Components/PageViewer/pageViewer'
+import { useLocation } from 'react-router-dom';
+import { Navbar } from '../Components/Navbar/navbar';
+import { Infobar } from '../Components/Infobar/infobar';
+import { PageViewer } from '../Components/PageViewer/pageViewer';
 
-import './pages.css'
+import './pages.css';
 
 export const PlayPage = () => {
+  // Get title and data from Mainpage
+  const location = useLocation();
+  const { title, data } = location.state;
 
-    // Get title and data from Mainpage
-    const location = useLocation()
-    const { title, data } = location.state
+  // Define page number
+  const [leftPageNum, setLeftPageNum] = useState(0);
+  const [rightPageNum, setRightPageNum] = useState(1);
+  const pageContainerRef = useRef(null);
+  const pageRef = useRef(null);
 
-    // Define page number
-    const [leftPageNum, setLeftPageNum] = useState(0)
-    const [rightPageNum, setRightPageNum] = useState(1)
-
-    function prevPage() {
-        if(leftPageNum > 0) {
-            setLeftPageNum(leftPageNum - 2);
-            setRightPageNum(rightPageNum - 2);            
-        }
+  function prevPage() {
+    if (leftPageNum > 0) {
+      setLeftPageNum(leftPageNum - 2);
+      setRightPageNum(rightPageNum - 2);
     }
+  }
 
-    function nextPage() {
-        if(data.length > rightPageNum ) {
-            setLeftPageNum(leftPageNum + 2);
-            setRightPageNum(rightPageNum + 2);            
-        }
+  function nextPage() {
+    if (data.length > rightPageNum) {
+      setLeftPageNum(leftPageNum + 2);
+      setRightPageNum(rightPageNum + 2);
     }
+  }
 
-    return(
-        <>
-            <Navbar title={title}/>
-            <div className='playContents'>
-                <div className='bookContainer'>
-                    <button className='btn' onClick={prevPage}> prev </button>
-                    <div className='leftPage'>
-                        <PageViewer num={leftPageNum} data={data}/>
-                    </div>
-                    <div className='rightPage'>
-                        <PageViewer num={rightPageNum} data={data}/>
-                    </div>
-                    <button className='btn' onClick={nextPage}> next </button>
-                </div>
-                <div>
-                    {title}
-                </div>
-                <Infobar/>          
+  return (
+    <div className='container'>
+      <Navbar title={title} />
+      <div className='playContents'>
+        <div className='bookContainer'>
+          <button className='btn' onClick={prevPage}>
+            <span>prev</span>
+          </button>
+          <div className='pageContainer' ref={pageContainerRef}>
+            <div className='pages' ref={pageRef}>
+              <PageViewer num={leftPageNum} data={data} />
+              <PageViewer num={rightPageNum} data={data} />
             </div>
-        </>
-    )
-}
+          </div>
+          <button className='btn' onClick={nextPage}>
+            <span>next</span>
+          </button>
+        </div>
+        <Infobar />
+      </div>
+    </div>
+  );
+};
