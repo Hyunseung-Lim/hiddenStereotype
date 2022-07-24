8dcf89a6e402b5634a04069676f4574b3681c173
Hyunseung Lim <charlie9807@kaist.ac.kr>
2022-07-25 00:22:28 +0900

    inital commit 5

--- client/src/Pages/mainPage.js
+++ client/src/Pages/mainPage.js
@@ -5,9 +5,18 @@ import data_2 from '../Data/book2'
 
 export const MainPage = () => {
     return(
-        <>
-            <Link to={'/play'} state={{title: "종이 봉지 공주", data: data_1}}> 종이 봉지 공주 </Link>
-            <Link to={'/play'} state={{title: "치과 의사 드소토 선생님", data: data_2}}> 치과 의사 드소토 선생님 </Link>
-        </>
+        <div className='mainPage'>
+            숨은 고정관념 찾기
+            <div className='bookBtnContainer'>
+                <Link className='bookBtn' to={'/play'} state={{title: "종이 봉지 공주", data: data_1}}>
+                    <img className='bookCover' src='book1/cover.jpg'/>
+                    종이 봉지 공주
+                </Link>
+                <Link className='bookBtn' to={'/play'} state={{title: "치과 의사 드소토 선생님", data: data_2}}>
+                    <img className='bookCover' src='book2/cover.jpg'/>
+                    치과 의사 드소토
+                </Link>
+            </div>
+        </div>
     )
 }
